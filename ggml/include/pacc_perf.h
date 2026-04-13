5c4b56593470ad387bc58cb0e0a30b1dd156fb84
chenqingyu <1549671870@qq.com>
2026-04-13 10:03:13 +0800

    update perf data

--- ggml/include/pacc_perf.h
+++ ggml/include/pacc_perf.h
@@ -3,18 +3,25 @@
 
 static void display_info(const struct ggml_tensor *t)
 {
+    GGML_LOG_INFO("ternsor name: %s, ", t->name);
     GGML_LOG_INFO("op type: %s, ", ggml_op_name(t->op));
-
     GGML_LOG_INFO("data type: %s, ", ggml_type_name(t->type));
-    GGML_LOG_INFO("dim0: %zu, dim1: %zu, dim2: %zu, dim3: %zu, ", t->ne[0], t->ne[1], t->ne[2], t->ne[3]);
+    GGML_LOG_INFO("ne:[dim0: %zu, dim1: %zu, dim2: %zu, dim3: %zu]", t->ne[0], t->ne[1], t->ne[2], t->ne[3]);
+    GGML_LOG_INFO("nb:[dim0: %zu, dim1: %zu, dim2: %zu, dim3: %zu]", t->nb[0], t->nb[1], t->nb[2], t->nb[3]);
+    GGML_LOG_INFO("\n");
     for (int i = 0; i < GGML_MAX_SRC; ++i) {
         if (t->src[i] == NULL) {
             break;
         }
         const struct ggml_tensor *tp = t->src[i];
-        GGML_LOG_INFO("src %d, ", i);
-        GGML_LOG_INFO("data type: %s, ", ggml_type_name(t->type));
-        GGML_LOG_INFO("dim0: %zu, dim1: %zu, dim2: %zu, dim3: %zu, ", tp->ne[0], tp->ne[1], tp->ne[2], tp->ne[3]);
+        GGML_LOG_INFO("  src%d---, ", i);
+        GGML_LOG_INFO("ternsor name: %s, ", tp->name);
+        GGML_LOG_INFO("op type: %s, ", ggml_op_name(tp->op));
+        GGML_LOG_INFO("data type: %s, ", ggml_type_name(tp->type));
+        GGML_LOG_INFO("ne:[dim0: %zu, dim1: %zu, dim2: %zu, dim3: %zu]", tp->ne[0], tp->ne[1], tp->ne[2], tp->ne[3]);
+        GGML_LOG_INFO("nb:[dim0: %zu, dim1: %zu, dim2: %zu, dim3: %zu]", tp->nb[0], tp->nb[1], tp->nb[2], tp->nb[3]);
+        GGML_LOG_INFO("\n");
     }
     GGML_LOG_INFO("\n");
 }
+		
