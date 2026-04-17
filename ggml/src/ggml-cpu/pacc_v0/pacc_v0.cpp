b8fd98f67b5f8d48b8be5839f0e3e054191e770c
heshan <heshan>
2026-04-17 11:09:36 +0800

    local mulmat test finish

--- ggml/src/ggml-cpu/pacc_v0/pacc_v0.cpp
+++ ggml/src/ggml-cpu/pacc_v0/pacc_v0.cpp
@@ -17,6 +17,8 @@
 #include <stdexcept>
 #include <thread>
 
+#include <utility>
+#include <riscv_vector.h>
 
 #if defined(__GNUC__)
 // #pragma GCC diagnostic ignored "-Woverlength-strings"
@@ -51,21 +53,129 @@ class tensor_traits_common : public tensor_traits_base {
         return false;
     }
 
+
     bool compute_forward(struct ggml_compute_params * params, struct ggml_tensor * op) override {
         struct PACC_fd * pacc_fd = pacc_v0_get_fds();
         switch (op->op) {
             case GGML_OP_MUL_MAT:
-                if (params->ith < pacc_fd->count) {
-                    printf("ith: %d, pacc_fd: %d\n", params->ith, pacc_fd->pacc_device_fds[params->ith]);
-                    pacc_v0_compute_forward_mul_mat(params, op);
+                if (op->src[0]->type == GGML_TYPE_F16 || op->src[0]->type == GGML_TYPE_BF16) {
+                    if (params->ith < pacc_fd->count) {
+                        printf("ith: %d, pacc_fd: %d\n", params->ith, pacc_fd->pacc_device_fds[params->ith]);
+                        pacc_v0_compute_forward_mul_mat(params, op);
+                    }
+                    return true;
                 }
-                return true;
                 break;
             default:
                 // GGML_ABORT("fatal error");
                 break;
         }
         return false;
+    }
+
+    enum LaunchKernelType {
+        LOCAL_TEST_FP16,
+        LOCAL_TEST_BF16,
+        PACC_FP16,
+        PACC_BF16,
+    };
+
+    static void my_vec_dot_f16(int n, float * __restrict__ s, const uint16_t * __restrict__ x, const uint16_t * __restrict__ y) {
+        float sumf = 0.0;
+
+        int vl = __riscv_vsetvlmax_e32m2();
+        vfloat32m1_t vs = __riscv_vfmv_v_f_f32m1(0.0f, 1);
+        vfloat32m2_t vsum;
+        vfloat16m1_t ax;
+        vfloat16m1_t ay;
+        vsum = __riscv_vreinterpret_v_u32m2_f32m2(__riscv_vmv_v_x_u32m2(0, vl));
+        for (int i = 0; i < n; i += vl) {
+            vl = __riscv_vsetvl_e16m1(n - i);
+            ax = __riscv_vle16_v_f16m1_tu(ax, (const _Float16 *)&x[i], vl);
+            ay = __riscv_vle16_v_f16m1_tu(ay, (const _Float16 *)&y[i], vl);
+            vsum = __riscv_vfwmacc_vv_f32m2_tu(vsum, ax, ay, vl);
+        }
+        vl = __riscv_vsetvlmax_e32m1();
+        vfloat32m1_t ac0 = __riscv_vfadd_vv_f32m1(__riscv_vget_v_f32m2_f32m1(vsum, 0), __riscv_vget_v_f32m2_f32m1(vsum, 1), vl);
+        vs = __riscv_vfredusum_vs_f32m1_f32m1(ac0, vs, vl);
+        sumf += __riscv_vfmv_f_s_f32m1_f32(vs);
+
+        *s = sumf;
+    }
+
+    static void my_vec_dot_bf16(int n, float * __restrict__ s, const uint16_t * __restrict__ x, const uint16_t * __restrict__ y) {
+        float sumf = 0.0;
+
+        int vl = __riscv_vsetvlmax_e32m2();
+        vfloat32m1_t vs = __riscv_vfmv_v_f_f32m1(0.0f, 1);
+        vfloat32m2_t vsum;
+        vbfloat16m1_t ax;
+        vbfloat16m1_t ay;
+        vsum = __riscv_vreinterpret_v_u32m2_f32m2(__riscv_vmv_v_x_u32m2(0, vl));
+        for (int i = 0; i < n; i += vl) {
+            vl = __riscv_vsetvl_e16m1(n - i);
+            ax = __riscv_vle16_v_bf16m1_tu(ax, (const __bf16 *)&x[i], vl);
+            ay = __riscv_vle16_v_bf16m1_tu(ay, (const __bf16 *)&y[i], vl);
+            vsum = __riscv_vfwmaccbf16_vv_f32m2_tu(vsum, ax, ay, vl);
+        }
+        vl = __riscv_vsetvlmax_e32m1();
+        vfloat32m1_t ac0 = __riscv_vfadd_vv_f32m1(__riscv_vget_v_f32m2_f32m1(vsum, 0), __riscv_vget_v_f32m2_f32m1(vsum, 1), vl);
+        vs = __riscv_vfredusum_vs_f32m1_f32m1(ac0, vs, vl);
+        sumf += __riscv_vfmv_f_s_f32m1_f32(vs);
+
+        *s = sumf;
+    }
+
+    void localKernelFP16(int m, int n, int k, const uint16_t *A, int lda, const uint16_t *B, int ldb, float *C, int ldc) {
+        for (int mm = 0; mm < m; ++mm) {
+            for (int nn = 0; nn < n; ++nn) {
+                my_vec_dot_f16(k, &C[mm + nn * m], &A[mm * k], &B[nn * k]);
+            }
+        }
+    }
+
+    void localKernelBF16(int m, int n, int k, const uint16_t *A, int lda, const uint16_t *B, int ldb, float *C, int ldc) {
+        for (int mm = 0; mm < m; ++mm) {
+            for (int nn = 0; nn < n; ++nn) {
+                my_vec_dot_bf16(k, &C[mm + nn * m], &A[mm * k], &B[nn * k]);
+            }
+        }
+    }
+
+    void pacc_mat_mul_wrapper(int m, int n, int k, const uint16_t *A, int lda, const uint16_t *B, int ldb, float *C, int ldc, int pacc_fd, enum LaunchKernelType t) {
+        if (t == LOCAL_TEST_FP16 || t == LOCAL_TEST_BF16) {
+            if (t == LOCAL_TEST_FP16) {
+                localKernelFP16(m, n, k, A, lda, B, ldb, C, ldc);
+            } else if (t == LOCAL_TEST_BF16) {
+                localKernelBF16(m, n, k, A, lda, B, ldb, C, ldc);
+            }
+        } else if (t == PACC_FP16 || t == PACC_BF16) {
+            std::pair<void*, create_bo> bufA;
+            int bufA_size = m * lda;
+            share_memory_alloc(&bufA.first, bufA_size, pacc_fd, &bufA.second);
+            std::pair<void*, create_bo> bufB;
+            int bufB_size = n * ldb;
+            share_memory_alloc(&bufB.first, bufB_size, pacc_fd, &bufB.second);
+            std::pair<void*, create_bo> bufC;
+            int bufC_size = m * ldc;
+            share_memory_alloc(&bufC.first, bufC_size, pacc_fd, &bufC.second);
+
+            memcpy(bufA.first, A, bufA_size);
+            memcpy(bufB.first, B, bufB_size);
+
+            pacc_error_t err;
+            if (t == PACC_FP16) {
+                err = pacc_mul_mat_f16(m, n, k, (const uint16_t *)bufA.first, lda, (const uint16_t *)bufB.first, ldb, (float *)bufC.first, ldc, pacc_fd, &bufA.second, &bufB.second, &bufC.second);
+            } else if (t == PACC_BF16) {
+                err = pacc_mul_mat_bf16(m, n, k, (const uint16_t *)bufA.first, lda, (const uint16_t *)bufB.first, ldb, (float *)bufC.first, ldc, pacc_fd, &bufA.second, &bufB.second, &bufC.second);
+            }
+
+            if (err != paccSuccess) {
+                GGML_ABORT("call pacc function with error: %d. ", err);
+            }
+
+            memcpy(C, bufC.first, bufC_size);
+        }
     }
 
     void pacc_v0_compute_forward_mul_mat(
@@ -87,45 +197,51 @@ class tensor_traits_common : public tensor_traits_base {
 
         struct PACC_fd * pacc_fd = pacc_v0_get_fds();
         if (src1_cont) {
+
+            int n = ggml_nelements(src1);
+            void * work_data = nullptr;
+            if (src0->type == GGML_TYPE_F16) {
+                work_data = new ggml_fp16_t[n];
+                ggml_fp32_to_fp16_row((float *)src1->data, (ggml_fp16_t *)work_data, n);
+            } if (src0->type == GGML_TYPE_BF16) {
+                work_data = new ggml_bf16_t[n];
+                ggml_fp32_to_bf16_row((float *)src1->data, (ggml_bf16_t *)work_data, n);
+            }
+
             for (int64_t i13 = 0; i13 < ne13; i13++) {
                 for (int64_t i12 = 0; i12 < ne12; i12++) {
-                    if (src1->type == GGML_TYPE_F16) {
-                        err = pacc_mul_mat_f16(
-                            ne01,
-                            ne11,
-                            ne00/ggml_blck_size(src0->type),
-                            (const uint16_t *)((const char *)src0->data + i12/r2*nb02 + i13/r3*nb03),
-                            nb01/ggml_type_size(src0->type) * ggml_type_size(src0->type),
-                            (const uint16_t *)((const char *)src1->data + i12*nb12 + i13*nb13),
-                            nb11/ggml_type_size(src1->type) * ggml_type_size(src1->type),
-                            (float *)((char *)dst->data + i12*nb2 + i13*nb3),
-                            nb1/ggml_type_size(dst->type) * ggml_type_size(src1->type),
-                            0,
-                            pacc_fd->pacc_device_fds[0]
-                           );
-                    } else if (src1->type == GGML_TYPE_BF16) {
-                        err = pacc_mul_mat_bf16(
-                            ne01,
-                            ne11,
-                            ne00/ggml_blck_size(src0->type),
-                            (const uint16_t *)((const char *)src0->data + i12/r2*nb02 + i13/r3*nb03),
-                            nb01/ggml_type_size(src0->type) * ggml_type_size(src1->type),
-                            (const uint16_t *)((const char *)src1->data + i12*nb12 + i13*nb13),
-                            nb11/ggml_type_size(src1->type) * ggml_type_size(src1->type),
-                            (float *)((char *)dst->data + i12*nb2 + i13*nb3),
-                            nb1/ggml_type_size(dst->type) * ggml_type_size(src1->type),
-                            0,
-                            pacc_fd->pacc_device_fds[0]
-                           );
+                    LaunchKernelType t = LOCAL_TEST_FP16;
+
+                    if (src0->type == GGML_TYPE_F16) {
+                        t = PACC_FP16;
+                    } else if (src0->type == GGML_TYPE_BF16) {
+                        t = PACC_BF16;
+                    } else {
+                        GGML_ABORT("unsupport datatype: %d. ", src0->type);
                     }
 
-                    if (err != paccSuccess) {
-                        GGML_ABORT("device mul mat error. ");
-                    }
+                    t = LOCAL_TEST_BF16;
+
+                    pacc_mat_mul_wrapper(
+                        ne01,
+                        ne11,
+                        ne00/ggml_blck_size(src0->type),
+                        (const uint16_t *)((const char *)src0->data + i12/r2*nb02 + i13/r3*nb03),
+                        nb01/ggml_type_size(src0->type) * ggml_type_size(src0->type),
+                        (const uint16_t *)((const char *)work_data + i12*nb12 + i13*nb13),
+                        nb11/ggml_type_size(src1->type) * ggml_type_size(src1->type),
+                        (float *)((char *)dst->data + i12*nb2 + i13*nb3),
+                        nb1/ggml_type_size(dst->type) * ggml_type_size(src1->type),
+                        pacc_fd->pacc_device_fds[0],
+                        t
+                    );
                 }
             }
 
+            delete[] work_data;
         }
+
+        ggml_barrier(params->threadpool);
     }
 
 
@@ -220,7 +336,7 @@ class extra_buffer_type : ggml::cpu::extra_buffer_type {
     bool supports_op(ggml_backend_dev_t, const struct ggml_tensor * op) override {
         switch (op->op) {
             case GGML_OP_MUL_MAT:
-                return true;
+                return false;
                 break;
             default:
                 // GGML_ABORT("fatal error");
--- tests/test-single-mulmat.cpp
+++ tests/test-single-mulmat.cpp
@@ -1,239 +1,128 @@
 #include "ggml.h"
 #include "ggml-cpu.h"
-
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <cassert>
+#include <string.h>
+#include <stdio.h>
 #include <vector>
 
-#if defined(_MSC_VER)
-#pragma warning(disable: 4244 4267) // possible loss of data
-#endif
+using namespace std;
 
-#if defined(__GNUC__)
-#pragma GCC diagnostic ignored "-Wdouble-promotion"
-#endif
-
-#define MAX_NARGS 3
-
-#undef MIN
-#undef MAX
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
-
-#define GGML_SILU_FP16
-
-//
-// logging
-//
-
-#if (GGML_DEBUG >= 1)
-#define GGML_PRINT_DEBUG(...) printf(__VA_ARGS__)
-#else
-#define GGML_PRINT_DEBUG(...)
-#endif
-
-#if (GGML_DEBUG >= 5)
-#define GGML_PRINT_DEBUG_5(...) printf(__VA_ARGS__)
-#else
-#define GGML_PRINT_DEBUG_5(...)
-#endif
-
-#if (GGML_DEBUG >= 10)
-#define GGML_PRINT_DEBUG_10(...) printf(__VA_ARGS__)
-#else
-#define GGML_PRINT_DEBUG_10(...)
-#endif
-
-#define GGML_PRINT(...) printf(__VA_ARGS__)
-
-static float frand(void) {
-    return (float)rand()/(float)RAND_MAX;
-}
-
-static int irand(int n) {
-    if (n == 0) return 0;
-    return rand()%n;
-}
-
-static void get_random_dims(int64_t * dims, int ndims) {
-    dims[0] = dims[1] = dims[2] = dims[3] = 1;
-
-    for (int i = 0; i < ndims; i++) {
-        dims[i] = 1 + irand(4);
-    }
-}
-
-static struct ggml_tensor * get_random_tensor_f32(
-        struct ggml_context * ctx0,
-        int ndims,
-        const int64_t ne[],
-        float fmin,
-        float fmax) {
-    struct ggml_tensor * result = ggml_new_tensor(ctx0, GGML_TYPE_F32, ndims, ne);
-
-    switch (ndims) {
-        case 1:
-            for (int i0 = 0; i0 < ne[0]; i0++) {
-                ((float *)result->data)[i0] = frand()*(fmax - fmin) + fmin;
-            }
-            break;
-        case 2:
-            for (int i1 = 0; i1 < ne[1]; i1++) {
-                for (int i0 = 0; i0 < ne[0]; i0++) {
-                    ((float *)result->data)[i1*ne[0] + i0] = frand()*(fmax - fmin) + fmin;
-                }
-            }
-            break;
-        case 3:
-            for (int i2 = 0; i2 < ne[2]; i2++) {
-                for (int i1 = 0; i1 < ne[1]; i1++) {
-                    for (int i0 = 0; i0 < ne[0]; i0++) {
-                        ((float *)result->data)[i2*ne[1]*ne[0] + i1*ne[0] + i0] = frand()*(fmax - fmin) + fmin;
-                    }
-                }
-            }
-            break;
-        case 4:
-            for (int i3 = 0; i3 < ne[3]; i3++) {
-                for (int i2 = 0; i2 < ne[2]; i2++) {
-                    for (int i1 = 0; i1 < ne[1]; i1++) {
-                        for (int i0 = 0; i0 < ne[0]; i0++) {
-                            ((float *)result->data)[i3*ne[2]*ne[1]*ne[0] + i2*ne[1]*ne[0] + i1*ne[0] + i0] = frand()*(fmax - fmin) + fmin;
-                        }
-                    }
-                }
-            }
-            break;
-        default:
-            assert(false);
-    };
-
-    return result;
-}
-
-static void ggml_graph_compute_helper(std::vector<uint8_t> & buf, ggml_cgraph * graph, int n_threads) {
-    struct ggml_cplan plan = ggml_graph_plan(graph, n_threads, nullptr);
-
-    if (plan.work_size > 0) {
-        buf.resize(plan.work_size);
-        plan.work_data = buf.data();
-    }
-
-    ggml_graph_compute(graph, &plan);
-}
-
-static void show_matmul_tensor_fp32(int64_t m, int64_t n, int64_t k,
-                                    const void *A, int64_t lda,
-                                    const void *B, int64_t ldb,
-                                    const void *C, int64_t ldc) {
-
-    printf("A:\n");
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < k; ++j) {
-            const float *pos = (const float *)A + i * lda + j;
-
-            printf("%f ", *pos);
-        }
-        printf("\n");
-    }
-    printf("B:\n");
-    for (int i = 0; i < k; ++i) {
-        for (int j = 0; j < n; ++j) {
-            const float *pos = (const float *)B + i + j * ldb;
-
-            printf("%f ", *pos);
-        }
-        printf("\n");
-    }
-    printf("C:\n");
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
-            const float *pos = (const float *)C + i + j * ldc;
-
-            printf("%f ", *pos);
-        }
-        printf("\n");
-    }
-}
-
-static void test_simple_matmul()
+void mul_mat_test_fp16(int m, int k, int n)
 {
+    // initialize data of matrices to perform matrix multiplication
+    const int rows_A = m, cols_A = k;
+
+    vector<float> matrix_A(rows_A * cols_A);
+    for (int i = 0; i < rows_A; ++i) {
+        for (int j = 0; j < cols_A; ++j) {
+            matrix_A.at(i * cols_A + j) = j;
+        }
+    }
+
+    const int rows_B = n, cols_B = k;
+
+    vector<float> matrix_B(rows_B * cols_B);
+    for (int i = 0; i < rows_B; ++i) {
+        for (int j = 0; j < cols_B; ++j) {
+            matrix_B.at(i * cols_B + j) = j;
+        }
+    }
+
+    // 1. Allocate `ggml_context` to store tensor data
+    // Calculate the size needed to allocate
+    size_t ctx_size = 0;
+    ctx_size += rows_A * cols_A * ggml_type_size(GGML_TYPE_F16); // tensor a
+    ctx_size += rows_B * cols_B * ggml_type_size(GGML_TYPE_F16); // tensor b
+    ctx_size += rows_A * rows_B * ggml_type_size(GGML_TYPE_F32); // result
+    ctx_size += 3 * ggml_tensor_overhead(); // metadata for 3 tensors
+    ctx_size += ggml_graph_overhead(); // compute graph
+    ctx_size += 1024; // some overhead (exact calculation omitted for simplicity)
+
+    // Allocate `ggml_context` to store tensor data
     struct ggml_init_params params = {
-        /* .mem_size   = */ ggml_tensor_overhead()*128 + ggml_graph_overhead(),
-        /* .mem_buffer = */ NULL,
-        /* .no_alloc   = */ false,
+        /*.mem_size =*/ ctx_size,
+        /*.mem_buffer =*/ NULL,
+        /*.no_alloc =*/ false,
     };
+    struct ggml_context * ctx = ggml_init(params);
 
-    std::vector<uint8_t> work_buffer;
+    // 2. Create tensors and set data
+    struct ggml_tensor * tensor_a = ggml_new_tensor_2d(ctx, GGML_TYPE_F16, cols_A, rows_A);
+    struct ggml_tensor * tensor_b = ggml_new_tensor_2d(ctx, GGML_TYPE_F16, cols_B, rows_B);
 
-    struct ggml_context * ctx0 = ggml_init(params);
+    ggml_fp32_to_fp16_row(matrix_A.data(), (ggml_fp16_t *)tensor_a->data, ggml_nelements(tensor_a));
+    ggml_fp32_to_fp16_row(matrix_B.data(), (ggml_fp16_t *)tensor_b->data, ggml_nelements(tensor_b));
 
-    struct ggml_tensor * tensor_a = ggml_new_tensor_2d(ctx0, GGML_TYPE_F32, 2, 4);
-    GGML_TENSOR_LOCALS(int32_t, a_ne, tensor_a, ne);
-    GGML_TENSOR_LOCALS(size_t, a_nb, tensor_a, nb);
+    // 3. Create a `ggml_cgraph` for mul_mat operation
+    struct ggml_cgraph * gf = ggml_new_graph(ctx);
 
-    float * tensor_a_data = (float *) tensor_a->data;
-    for (int64_t i1 = 0; i1 < a_ne1; i1++) {
-        for (int64_t i0 = 0; i0 < a_ne0; i0++) {
-            int64_t idx = (i0 + i1 * a_ne0);
-            if ((i1 + i0) % 2 == 0) {
-                tensor_a_data[idx] = 0;
-            } else {
-                tensor_a_data[idx] = 1;
-            }
+    // result = a*b^T
+    // Pay attention: ggml_mul_mat(A, B) ==> B will be transposed internally
+    // the result is transposed
+    struct ggml_tensor * result = ggml_mul_mat(ctx, tensor_a, tensor_b);
+
+    // Mark the "result" tensor to be computed
+    ggml_build_forward_expand(gf, result);
+
+    // 4. Run the computation
+    int n_threads = 1; // Optional: number of threads to perform some operations with multi-threading
+    ggml_graph_compute_with_ctx(ctx, gf, n_threads);
+
+    // 5. Retrieve results (output tensors)
+    float * result_data = (float *) result->data;
+    printf("m:%d, n:%d, k:%d\n, mul mat (%d x %d) (transposed result):\n[", m, n, k, (int) result->ne[0], (int) result->ne[1]);
+    for (int j = 0; j < result->ne[1]/* rows */; j++) {
+        if (j > 0) {
+            printf("\n");
+        }
+
+        for (int i = 0; i < result->ne[0]/* cols */; i++) {
+            printf(" %.2f", result_data[j * result->ne[0] + i]);
         }
     }
-    int n = ggml_nelements(tensor_a);
-    for (int i = 0; i < n; ++i) {
-        // tensor_a_data[i] = (i % 2);
-    }
+    printf(" ]\n");
 
-    struct ggml_tensor * tensor_b = ggml_new_tensor_2d(ctx0, GGML_TYPE_F32, 2, 3);
-    GGML_TENSOR_LOCALS(int32_t, b_ne, tensor_b, ne);
-    GGML_TENSOR_LOCALS(size_t, b_nb, tensor_b, nb);
-
-    float * tensor_b_data = (float *) tensor_b->data;
-    int v = 0;
-    for (int64_t i1 = 0; i1 < b_ne1; i1++) {
-        for (int64_t i0 = 0; i0 < b_ne0; i0++) {
-            int64_t idx = (i0 + i1 * b_ne0);
-            tensor_b_data[idx] = v++;
+    for (int i = 1; i < ggml_nelements(result); ++i) {
+        if (result_data[i] != result_data[i - 1]) {
+            printf("result error, m: %d, n: %d, k: %d\n", m, n, k);
+            break;
         }
     }
 
-    int nn = ggml_nelements(tensor_a);
-    for (int i = 0; i < nn; ++i) {
-        tensor_b_data[i] = i;
-    }
-
-    struct ggml_tensor * r0 = ggml_mul_mat(ctx0, tensor_a, tensor_b);
-
-    ggml_cgraph * gf = ggml_new_graph(ctx0);
-
-    ggml_build_forward_expand(gf, r0);
-
-    ggml_graph_compute_helper(work_buffer, gf, 1);
-
-    const float * r0_data = (float *) r0->data;
-    GGML_TENSOR_LOCALS(int32_t, r0_ne, r0, ne);
-
-    printf("ne1: %d, ne0: %d\n", r0_ne1, r0_ne0);
-    for (int64_t i0 = 0; i0 < r0_ne0; i0++) {
-        for (int64_t i1 = 0; i1 < r0_ne1; i1++) {
-            // int64_t idx = (i0 * b_nb0 + i1 * b_nb1);
-            int64_t idx = (4 * i1 + i0);
-            printf("%d ", (int)r0_data[idx]);
-        }
-        printf("\n");
-    }
-
-    ggml_free(ctx0);
+    // 6. Free memory and exit
+    ggml_free(ctx);
 }
 
-int main(int /*argc*/, const char ** /*argv*/) {
-    test_simple_matmul();
+struct DimsInfo
+{
+    int m = 0;
+    int n = 0;
+    int k = 0;
+    int counter = 0;
 
+    DimsInfo(int mm, int nn, int kk, int cc)
+    :m(mm), n(nn), k(kk), counter(cc) {}
+};
+
+
+int main(void) {
+    int seq_len = 8;
+    vector<DimsInfo> dimsInfo = {
+        { seq_len, 2048, 2048, 44, },
+        { seq_len, 256, 2048, 44, },
+        { seq_len, 5632, 2048, 44, },
+        { seq_len, 256, 64, 22, },
+        { seq_len, 64, 256, 22, },
+        { seq_len, 2048, 5632, 22, },
+        { seq_len, 32000, 2048, 1, },
+    };
+    /*
+    vector<DimsInfo> dimsInfo = {
+        { 1, 4, 3, 1, },
+    };
+    */
+    for (auto [m, n, k, c] : dimsInfo) {
+        mul_mat_test_fp16(m, k, n);
+    }
     return 0;
 }
+
