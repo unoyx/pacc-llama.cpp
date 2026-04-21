d46989a0b40d75f1256684045c4ebcbfd511a468
chenqingyu <1549671870@qq.com>
2026-04-21 19:41:07 +0800

    embd op

--- src/llama-model-loader.h
+++ src/llama-model-loader.h
@@ -61,6 +61,7 @@ struct llama_model_loader {
     static const int TENSOR_NOT_REQUIRED = 1 << 0;
     static const int TENSOR_DUPLICATED   = 1 << 1;
     static const int TENSOR_SKIP         = 1 << 2;
+    static const int TENSOR_COPY   	 = 1 << 3;
 
     int n_kv      = 0;
     int n_tensors = 0;
